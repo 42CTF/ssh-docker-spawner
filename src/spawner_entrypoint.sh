900559c5a25834a477dba07445d9b3047e20ab8d
riblanc <richard.blc25@gmail.com>
2025-02-08 02:12:03 +0100

    add: restricts user to 1 running container

--- src/spawner_entrypoint.sh
+++ src/spawner_entrypoint.sh
@@ -15,6 +15,29 @@ services:
       - 'TOKEN=$TOKEN'
 EOF
 
-#docker --log-level=warning compose -f compose.yml -f label.yml down $USER --remove-orphans
+ALREADY_RUNNING=$(docker ps -q -f label=TOKEN=$TOKEN)
+
+if [ -n "$ALREADY_RUNNING" ]; then
+  echo "Warning: You already have one or more already running containers."
+
+  prompt="Do you want to stop them and start a new one? [y/N] "
+  read -r -p "$prompt" response
+
+  case "$response" in
+    [yY][eE][sS]|[yY])
+      for CONTAINER in $ALREADY_RUNNING; do
+	echo -en "Stopping container $CONTAINER \033[s... (this may take up to 10 seconds)"
+        container=$(docker stop $CONTAINER &)
+	# restore cursor position, clear the rest of the line and print OK
+	echo -e "\033[u\033[KOK"
+      done
+      ;;
+    *)
+      echo "Exiting..."
+      exit 0
+      ;;
+  esac
+fi
+
 docker --log-level=warning compose -f compose.yml -f label.yml run --rm $USER
 
