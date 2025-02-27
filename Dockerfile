f32643fd9e45040e8713744fff75270dd4744c08
riblanc <richard.blc25@gmail.com>
2025-02-27 03:10:27 +0100

    rework: PAM module
    This commit replaces the build script with a Makefile.
    It also remove the hardcoded 42CTF auth, and put the http part in comments.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /app
 
 RUN apk add --no-cache openssh-server-pam python3 py3-pip && pip install pyyaml supervisor --break-system-packages
 
+# packages to build PAM module
+RUN apk add --no-cache build-base linux-pam-dev curl-dev
+
 RUN mkdir -p /etc/ssh/sshd_config.d
 
 COPY . /app
--- app/PAM/pam.c
+++ app/PAM/pam.c
@@ -5,65 +5,83 @@
 #include <curl/curl.h>
 
 void curl_cleanup(CURL **curl) {
-	if (*curl) {
-		curl_easy_cleanup(*curl);
-	}
+    if (*curl) {
+        curl_easy_cleanup(*curl);
+    }
 }
 
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	const char *username;
-	int retval;
+    const char *username;
+    int retval;
 
-	CURL *curl __attribute__((cleanup(curl_cleanup))) = curl_easy_init();
-	CURLcode res;
+    // Retrieve the username
+    retval = pam_get_user(pamh, &username, NULL);
+    if (retval != PAM_SUCCESS) {
+        pam_error(pamh, "ERROR: Unable to retrieve username");
+        return retval;
+    }
 
-	if (!curl) return PAM_AUTH_ERR;
+    char *token = NULL;
+    pam_prompt(pamh, PAM_PROMPT_ECHO_ON, &token, "token: ");
 
-	// Retrieve the username
-	retval = pam_get_user(pamh, &username, NULL);
-	if (retval != PAM_SUCCESS) {
-		pam_error(pamh, "ERROR: Unable to retrieve username");
-		return retval;
-	}
+    const unsigned short TOKEN_LEN = 32;
 
-	char *token = NULL;
-	pam_prompt(pamh, PAM_PROMPT_ECHO_ON, &token, "42CTF token: ");
+    /*
+     * Here is an example of a working HTTP based authentication using a 32 character Bearer token
+     *
+     * // Retrieve user profile from the API
+     * const char *url = "https://your_website.com/your_endpoint";
 
-	// Retrieve user profile from the API
-	const char *url = "https://42ctf.org/en/accounts/me";
+     * CURL *curl __attribute__((cleanup(curl_cleanup))) = curl_easy_init();
+     * CURLcode res;
 
-	// Set the Authorization header
-	struct curl_slist *headers = NULL;
-	char auth_header[80];
-	snprintf(auth_header, sizeof(auth_header), "Authorization: %s", token);
+     * if (!curl) return PAM_AUTH_ERR;
 
-	headers = curl_slist_append(headers, auth_header);
-	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+     * // Prepare the Authorization header for curl
+     * struct curl_slist *headers = NULL;
+     * const char token_header_name[] = "Authorization";
+     * const char token_prefix[] = "Bearer ";
 
-	curl_easy_setopt(curl, CURLOPT_URL, url);
-	res = curl_easy_perform(curl);
+     * char auth_header[(sizeof(token_header_name)-1) + 2 + (sizeof(token_prefix)-1) + TOKEN_LEN + 1];
+     * bzero(auth_header, sizeof(auth_header));
 
-	if (res != CURLE_OK) {
-		pam_error(pamh, "ERROR: Failed to reach the 42CTF server");
-		return PAM_AUTH_ERR;
-	}
+     * snprintf(auth_header, sizeof(auth_header), "%s: %s%s", token_header_name, token_prefix, token);
 
-	// check if it's not a 200 status code
-	long http_code = 0;
-	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-	if (http_code != 200) {
-		pam_error(pamh, "ERROR: Invalid token");
-		return PAM_AUTH_ERR;
-	}
+     * // Setting the curl options
+     * headers = curl_slist_append(headers, auth_header);
+     * curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+     * curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+     * curl_easy_setopt(curl, CURLOPT_URL, url);
 
-	char env_token[39];
-	snprintf(env_token, sizeof(env_token), "TOKEN=%s", token);
+     * // performing the request
+     * res = curl_easy_perform(curl);
 
-	pam_putenv(pamh, env_token);
-	return PAM_SUCCESS;
+     * if (res != CURLE_OK) {
+     *     pam_error(pamh, "ERROR: Failed to reach server");
+     *     return PAM_AUTH_ERR;
+     * }
+
+     * // check if it's not a 200 status code
+     * long http_code = 0;
+     * curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+     * if (http_code != 200) {
+     *     pam_error(pamh, "ERROR: Invalid token");
+     *     return PAM_AUTH_ERR;
+     * }
+     */
+
+    // Set the TOKEN environment variable,
+    // (this variable must be set if you wanna constraint the user to only run one container at a time)
+    char env_token[TOKEN_LEN + 7];
+    bzero(env_token, sizeof(env_token));
+
+    snprintf(env_token, sizeof(env_token), "TOKEN=%s", token);
+    pam_putenv(pamh, env_token);
+
+    return PAM_SUCCESS;
 }
 
 int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	return PAM_SUCCESS;
+    return PAM_SUCCESS;
 }
 
